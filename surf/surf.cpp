6f6b52095a45832d8ee7489a8adadf4292eb4efb
Pratyush Kar <pratyush.kar@gmail.com>
2013-11-16 23:00:19 +0530

    added basic implementation of SURF

---
+++ surf/surf.cpp
@@ -0,0 +1,225 @@
+#include <stdio.h>
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <ueye.h>
+// #include "opencv2/nonfree/nonfree.hpp"
+
+using namespace cv;
+using namespace std;
+
+char* imgPointer = NULL;
+int imgMemPointer;
+
+int initializeCam(HIDS hCam)
+{   
+    char* errMsg = (char*)malloc(sizeof(char)*200);
+    int err = 0;
+
+    int nRet = is_InitCamera (&hCam, NULL);
+    if (nRet != IS_SUCCESS)
+    {
+        is_GetError (hCam, &err, &errMsg);
+        printf("Camera Init Error %d: %s\n",err,errMsg);
+        return EXIT_FAILURE;
+    }
+
+    nRet = is_SetColorMode(hCam, IS_CM_BGR8_PACKED);
+    if (nRet != IS_SUCCESS) 
+    {
+            is_GetError (hCam, &err, &errMsg);
+            printf("Color Mode Error %d: %s\n",err,errMsg);
+            return EXIT_FAILURE;  
+    }
+
+    nRet = is_SetHardwareGain(hCam, 100, 4, 0, 13);
+    if (nRet != IS_SUCCESS) 
+    {
+            is_GetError (hCam, &err, &errMsg);
+            printf("Hardware Gain Error %d: %s\n",err,errMsg);
+            return EXIT_FAILURE;
+    }
+
+    cout<<"init success\n";
+    return EXIT_SUCCESS;
+}
+
+int setImgMem(HIDS hCam)
+{
+    char* errMsg = (char*)malloc(sizeof(char)*200);
+    int err = 0;
+
+    int nRet = is_AllocImageMem(hCam, 752, 480, 24, &imgPointer, &imgMemPointer);
+    if(nRet != IS_SUCCESS)
+    {
+        is_GetError (hCam, &err, &errMsg);
+        printf("MemAlloc Unsuccessful %d: %s\n",err,errMsg);
+        return EXIT_FAILURE;
+    }
+
+    nRet = is_SetImageMem (hCam, imgPointer, imgMemPointer);
+    if(nRet != IS_SUCCESS)
+    {
+        is_GetError (hCam, &err, &errMsg);
+        printf("Could not set/activate image memory %d: %s\n",err, errMsg);
+        return EXIT_FAILURE;
+    }
+
+    cout<<"imgmem success\n";
+    return EXIT_SUCCESS;
+}
+
+void getFrame(HIDS hCam, Mat frame)
+{
+    char* errMsg = (char*)malloc(sizeof(char)*200);
+    int err = 0;
+
+
+    int nRet = is_FreezeVideo (hCam, IS_WAIT) ;
+    if(nRet != IS_SUCCESS)
+    {
+        is_GetError (hCam, &err, &errMsg);
+        printf("Could not grab image %d: %s\n",err,errMsg);
+        //return EXIT_FAILURE;
+    }
+        
+    //fill in the OpenCV imaga data 
+    //IplImage* img_rgb = cvCreateImage(cvSize(752,480),8,3);
+    memcpy(frame.data, imgPointer, 752*480 * 1);
+    //img_rgb->imageData = imgPointer;
+    //return img_rgb;
+    cout<<"frame success\n";
+}
+
+int exitCam(HIDS hCam)
+{
+    int nRet = is_ExitCamera (hCam);
+    if(nRet != IS_SUCCESS)
+    {
+        printf("Could not exit camera \n");
+        return EXIT_FAILURE;
+    }
+}
+
+void readme();
+
+int main()
+{
+  // if( argc != 3 )
+  // { readme(); return -1; }
+
+  // // //INITIALIZE CAMERA
+  // HIDS hCam = 1;
+  // initializeCam(hCam);
+  // setImgMem(hCam);
+  // cout<<"here\n";
+  while(1)
+  {
+    // cout<<"in while";
+    // Mat frame;
+    // Mat img_scene;
+    // getFrame(hCam,frame);
+    // cvtColor(frame,img_scene,CV_BGR2GRAY);
+    // imwrite("testcase.bmp",frame);
+    Mat img_object = imread( "test.bmp", CV_LOAD_IMAGE_GRAYSCALE );
+    Mat img_scene = imread( "testcase.bmp", CV_LOAD_IMAGE_GRAYSCALE );
+
+    if( !img_object.data || !img_scene.data )
+    { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+
+    //-- Step 1: Detect the keypoints using SURF Detector
+    int minHessian = 400;
+
+    SurfFeatureDetector detector( minHessian );
+
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
+
+    detector.detect( img_object, keypoints_object );
+    detector.detect( img_scene, keypoints_scene );
+
+    //-- Step 2: Calculate descriptors (feature vectors)
+    SurfDescriptorExtractor extractor;
+
+    Mat descriptors_object, descriptors_scene;
+
+    extractor.compute( img_object, keypoints_object, descriptors_object );
+    extractor.compute( img_scene, keypoints_scene, descriptors_scene );
+
+    //-- Step 3: Matching descriptor vectors using FLANN matcher
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match( descriptors_object, descriptors_scene, matches );
+
+    double max_dist = 0; double min_dist = 100;
+
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { double dist = matches[i].distance;
+      if( dist < min_dist ) min_dist = dist;
+      if( dist > max_dist ) max_dist = dist;
+    }
+
+    printf("-- Max dist : %f \n", max_dist );
+    printf("-- Min dist : %f \n", min_dist );
+
+    //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+    std::vector< DMatch > good_matches;
+
+    for( int i = 0; i < descriptors_object.rows; i++ )
+    { if( matches[i].distance < 3*min_dist )
+       { good_matches.push_back( matches[i]); }
+    }
+
+    Mat img_matches;
+    drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
+                 good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                 vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+    //-- Localize the object
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
+
+    for( int i = 0; i < good_matches.size(); i++ )
+    {
+      //-- Get the keypoints from the good matches
+      obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+      scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+    }
+
+    Mat H = findHomography( obj, scene, CV_RANSAC );
+
+    //-- Get the corners from the image_1 ( the object to be "detected" )
+    std::vector<Point2f> obj_corners(4);
+    obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
+    obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
+    std::vector<Point2f> scene_corners(4);
+
+    perspectiveTransform( obj_corners, scene_corners, H);
+
+    //-- Draw lines between the corners (the mapped object in the scene - image_2 )
+    line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
+    line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+
+    //-- Show detected matches
+    imshow( "Good Matches & Object detection", img_matches );
+    int c = cvWaitKey(1000);
+    if(c == 27)
+      break;
+  }
+
+    // waitKey(100);
+  cout<<"exit\n";
+  return 0;
+  }
+
+  /** @function readme */
+  void readme()
+  { std::cout << " Usage: ./SURF_descriptor <img1> <img2>" << std::endl; }
