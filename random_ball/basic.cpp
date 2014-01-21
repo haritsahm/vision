3ad5950a0974dc98c949020ae763135c48e50291
p-kar <pratyush.kar@gmail.com>
2014-01-21 19:09:52 +0530

    created a basic code for circle detection on a backgroud

--- random_ball/basic.cpp
+++ random_ball/basic.cpp
@@ -76,7 +76,7 @@ int setImgMem(HIDS hCam)
     return EXIT_SUCCESS;
 }
 
-bool getFrame(HIDS hCam, Mat frame)
+bool getFrame(HIDS hCam, IplImage* frame)
 {
     char* errMsg = (char*)malloc(sizeof(char)*200);
     int err = 0;
@@ -91,7 +91,7 @@ bool getFrame(HIDS hCam, Mat frame)
     }
     else
     {
-        memcpy(frame.data, imgPointer, 752*480 * 3);
+        memcpy(frame->imageData, imgPointer, 752*480 * 3);
         return CAM_SUCCESS;  
     }      
     //fill in the OpenCV imaga data 
@@ -109,6 +109,13 @@ int exitCam(HIDS hCam)
         return EXIT_FAILURE;
     }
 }
+typedef unsigned char uchar;
+#define returnPixel1C(image, x, y) ((uchar*)(image->imageData + image->widthStep*(y)))[x]
+
+inline uchar pixelColor1C(IplImage* image, int x, int y)
+{
+    return ((uchar*)(image->imageData + image->widthStep*y))[x];
+}
 
 int main()
 {
@@ -116,5 +123,47 @@ int main()
     HIDS hCam = 1;
     initializeCam(hCam);
     setImgMem(hCam);
-    
+
+    while(1)
+    {
+        IplImage* frame = cvCreateImage(cvSize(752,480),8,3);
+        IplImage* img_hsv = cvCreateImage(cvSize(752,480),8,3);
+        IplImage* threshy = cvCreateImage(cvSize(752,480),8,1);
+        getFrame(hCam, frame);
+        cvCvtColor(frame,img_hsv,CV_BGR2HSV);
+        cvInRangeS(img_hsv, cvScalar(0, 120, 100), cvScalar(255, 255, 255), threshy);
+        cvSmooth(threshy,threshy,CV_MEDIAN,7,7);
+        for(int i = 0 ; i < 752 ; i++)
+        {
+            for(int j = 0 ; j < 480 ; j++)
+                {
+                    if(returnPixel1C(threshy,i,j) == 0)
+                        returnPixel1C(threshy,i,j) = 255;
+                    else
+                        returnPixel1C(threshy,i,j) = 0;
+                }
+        }
+        CvMemStorage* storage = cvCreateMemStorage(0);
+        CvSeq* results = cvHoughCircles(threshy,storage,CV_HOUGH_GRADIENT,1,threshy->width/10,100,20,0,100);
+        for( int i = 0; i < results->total; i++ ) 
+        {
+            float* p = (float*) cvGetSeqElem( results, i );
+            CvPoint pt = cvPoint( cvRound( p[0] ), cvRound( p[1] ) );
+            cvCircle(frame,pt,5,cvScalar(255,255,0),2,8);
+            cvCircle(frame,pt,cvRound( p[2] ),CV_RGB(0,0,255),CV_FILLED);
+            printf("FOUND\n");
+        }
+
+
+
+        cvShowImage("frame",frame);
+        cvShowImage("threshed",threshy);
+        int c = cvWaitKey(10);
+        if( c == 27 )
+            break;
+        cvReleaseImage(&frame);
+        cvReleaseImage(&img_hsv);
+        cvReleaseImage(&threshy);
+    }
+    return 0;
 }
