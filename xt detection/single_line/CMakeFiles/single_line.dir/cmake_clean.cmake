ea3ffe11f740e4212f594f7855820d2c0762f6a1
Pratyush Kar <pratyush.kar@gmail.com>
2014-03-14 08:51:36 +0530

    fixed conflicts

---
+++ xt detection/single_line/CMakeFiles/single_line.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/single_line.dir/single_line.cpp.o"
+  "single_line.pdb"
+  "single_line"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/single_line.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
